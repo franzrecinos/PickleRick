c66eddcb420f4fde00768a4c01d6288b1285785d
franzrecinos <franz.recinos@trov.com>
2018-07-31 15:55:58 -0400

    sorting function

--- src/services/index.js
+++ src/services/index.js
@@ -18,6 +18,29 @@ var getJobs = function getJobs(cb){
 
 }
 
+// var sortJobsBySalary = fucntion(jobs){
+//   var sortedObj = {},
+//         keys = Object.keys(jobs);
+
+//     keys.sort(function(key1, key2){
+//         key1 = key1.toLowerCase(), key2 = key2.toLowerCase();
+//         if(key1 < key2) return -1;
+//         if(key1 > key2) return 1;
+//         return 0;
+//     });
+
+//     for(var index in keys){
+//         var key = keys[index];
+//         if(typeof object[key] == 'object' && !(object[key] instanceof Array)){
+//             sortedObj[key] = sortObject(object[key]);
+//         } else {
+//             sortedObj[key] = object[key];
+//         }
+//     }
+
+//     return sortedObj;
+// }
+
 var serviceObject = {
   "getJobs":getJobs,
 }
